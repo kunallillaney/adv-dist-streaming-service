cd5753c055f2bc84154a59a18a8f9df5483c492d
raghuram.sv@gmail.com <raghuram.sv@gmail.com@817decba-a3b9-ad42-0e79-1bb7c488974d>
2013-04-23 04:54:27 +0000

    Adding TokenMgr

---
+++ webserver/src/com/jhu/ads/webserver/TokenMgr.java
@@ -0,0 +1,21 @@
+package com.jhu.ads.webserver;
+
+public class TokenMgr implements Runnable {
+    
+    public TokenMgr getInstance() {
+        return null; // TODO
+        
+        // Upon initialization -
+        // Should start the thread
+        // Should request for tokens from each Data Center 
+    }
+    
+    public void requestTokens(DataCenter dataCenter) {
+        // TODO
+    }
+    
+    public void run() {
+        // TODO
+    };
+
+}
