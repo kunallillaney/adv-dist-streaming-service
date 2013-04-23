48c41f3b19c1daf574905c1b8ab9240c592e74a5
raghuram.sv@gmail.com <raghuram.sv@gmail.com@817decba-a3b9-ad42-0e79-1bb7c488974d>
2013-04-23 04:39:52 +0000

    Added servlets

---
+++ trunk/webserver/src/com/jhu/ads/webserver/servlet/InitServlet.java
@@ -0,0 +1,14 @@
+package com.jhu.ads.webserver.servlet;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+public class InitServlet extends HttpServlet {
+    
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        // TODO Auto-generated method stub
+    }
+    
+}
