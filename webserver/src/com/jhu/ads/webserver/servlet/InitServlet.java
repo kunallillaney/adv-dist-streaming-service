23f049f8058189e894a11c74a58770e91ed871b4
raghuram.sv@gmail.com <raghuram.sv@gmail.com@817decba-a3b9-ad42-0e79-1bb7c488974d>
2013-04-24 07:15:46 +0000

    removed dhome

--- webserver/src/com/jhu/ads/webserver/servlet/InitServlet.java
+++ webserver/src/com/jhu/ads/webserver/servlet/InitServlet.java
@@ -22,17 +22,17 @@ public class InitServlet extends HttpServlet {
     public void init(ServletConfig config) throws ServletException {
         // TODO Auto-generated method stub
         
-        String dHome = System.getenv("DHOME");
+/*        String dHome = System.getenv("DHOME");
         if(dHome == null || dHome.equals("")) {
             throw new ServletException("DHOME environment variable NOT set.");
-        }
+        }*/
         
         // String path = config.getServletContext().getRealPath("/WEB-INF");
-        ConfigMgr.getInstance().init(dHome + "/webserver-config.properties");
+        ConfigMgr.getInstance().init("/home/klillan1/conf/" + "/webserver-config.properties");
         
         InputStream is;
         try {
-            is = new FileInputStream(dHome + "/datacenter-config.xml");
+            is = new FileInputStream("/home/klillan1/conf/" + "/datacenter-config.xml");
             DataCenterMgr.getInstance().init(is);
             is.close();
         } catch (FileNotFoundException e) {
