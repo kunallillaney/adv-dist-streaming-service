3c5f848ad7a7ae8de4ccddbc2f332e27ef3c1f6a
raghuram.sv@gmail.com <raghuram.sv@gmail.com@817decba-a3b9-ad42-0e79-1bb7c488974d>
2013-04-24 06:31:01 +0000

    Added initialization and glue code

--- trunk/webserver/src/com/jhu/ads/webserver/servlet/InitServlet.java
+++ trunk/webserver/src/com/jhu/ads/webserver/servlet/InitServlet.java
@@ -1,14 +1,47 @@
 package com.jhu.ads.webserver.servlet;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+
+import com.jhu.ads.webserver.DataCenterMgr;
+import com.jhu.ads.webserver.GeoIPMgr;
+import com.jhu.ads.webserver.common.ConfigMgr;
 
 public class InitServlet extends HttpServlet {
     
     @Override
     public void init(ServletConfig config) throws ServletException {
         // TODO Auto-generated method stub
+        
+        String dHome = System.getenv("DHOME");
+        if(dHome == null || dHome.equals("")) {
+            throw new ServletException("DHOME environment variable NOT set.");
+        }
+        
+        // String path = config.getServletContext().getRealPath("/WEB-INF");
+        ConfigMgr.getInstance().init(dHome + "/webserver-config.properties");
+        
+        InputStream is;
+        try {
+            is = new FileInputStream(dHome + "/datacenter-config.xml");
+            DataCenterMgr.getInstance().init(is);
+            is.close();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+        // TokenMgr.getInstance().init();
+        GeoIPMgr.getInstance().init(ConfigMgr.getInstance().getGeoIPFilePath());
     }
     
 }
--- trunk/webserver/src/com/jhu/ads/webserver/servlet/UserRequestServlet.java
+++ trunk/webserver/src/com/jhu/ads/webserver/servlet/UserRequestServlet.java
@@ -1,19 +1,45 @@
 package com.jhu.ads.webserver.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.jhu.ads.webserver.DataCenter;
+import com.jhu.ads.webserver.DataCenterMgr;
+import com.jhu.ads.webserver.GeoIPMgr;
+
+@WebServlet("/Request")
 public class UserRequestServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        super.doGet(req, resp);
+        System.out.println("Remote Addr: " + req.getRemoteAddr());
+        int zipCode = GeoIPMgr.getInstance().getZipCode(req.getRemoteAddr());
+        System.out.println("Zip Code: " + zipCode);
+        
+        DataCenter dataCenter = DataCenterMgr.getInstance().getDataCenter(zipCode);
+        int tokenNum = dataCenter.getAndIncrementCurrentToken();
+        String token = dataCenter.buildToken(tokenNum);
+        
+        System.out.println("Data Center IP: "+dataCenter.getControllerIP());
+        System.out.println("Token: "+token);
+        
+        resp.setContentType("text/html");
+        PrintWriter printWriter  = resp.getWriter();
+        printWriter.println("<h1>" + dataCenter.getControllerIP() + "</h1>");
+        printWriter.println("<h1>" + token + "</h1>");
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        doGet(req, resp);
     }
     
 }
