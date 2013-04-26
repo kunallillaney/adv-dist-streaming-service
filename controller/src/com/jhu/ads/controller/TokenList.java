aad2ae710329ef595d693f53d5b6f1d603a96f5a
p.adityarao@gmail.com <p.adityarao@gmail.com@817decba-a3b9-ad42-0e79-1bb7c488974d>
2013-04-26 10:35:34 +0000

    Removed TokenList

--- controller/src/com/jhu/ads/controller/TokenList.java
+++
@@ -1,9 +0,0 @@
-package com.jhu.ads.controller;
-
-import java.util.LinkedList;
-
-public class TokenList {
-
-	Token lastReceivedUpto = new Token();
-	LinkedList<Token> tokenList = new LinkedList<Token>();
-}
