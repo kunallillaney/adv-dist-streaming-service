253c5203e4dc03b3215234d1a590ec29f08e4126
raghuram.sv@gmail.com <raghuram.sv@gmail.com@817decba-a3b9-ad42-0e79-1bb7c488974d>
2013-04-26 07:08:50 +0000

    Added ConfigMgr.java

---
+++ controller/src/com/jhu/ads/controller/common/ConfigMgr.java
@@ -0,0 +1,58 @@
+package com.jhu.ads.controller.common;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigMgr {
+    
+    private static volatile ConfigMgr _instance = null;
+    
+    private static String SPREAD_DEAMON_ADDR_PROPERTY = "SpreadDeamonAddr";
+    private static String SPREAD_DEAMON_PORT_PROPERTY = "SpreadDeamonPort";
+    private static String DATA_CENTER_NAME_PROPERTY = "DataCenterName";
+    
+    private Properties props;
+    
+    public static ConfigMgr getInstance() {
+        if(_instance == null) {
+            synchronized (ConfigMgr.class) {
+                if(_instance == null) {
+                    _instance = new ConfigMgr();
+                }
+            }
+        }
+        return _instance;
+    }    
+    
+    public void init(String filePath) {
+        props = new Properties();
+        InputStream fin;
+        try {
+            fin = new FileInputStream(filePath);
+            props.load(fin);
+            fin.close();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    
+    public String getSpreadDeamonAddress() {
+        return props.getProperty(SPREAD_DEAMON_ADDR_PROPERTY);
+    }
+    
+    public int getSpreadDeamonPort() {
+        return Integer.parseInt(props.getProperty(SPREAD_DEAMON_PORT_PROPERTY));
+    }
+
+    public String getDataCenterName() {
+        return props.getProperty(DATA_CENTER_NAME_PROPERTY);
+    }
+    
+}
