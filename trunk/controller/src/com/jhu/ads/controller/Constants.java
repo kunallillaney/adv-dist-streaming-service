87da78d00a1415629311076ff8af76a10cf2f704
raghuram.sv@gmail.com <raghuram.sv@gmail.com@817decba-a3b9-ad42-0e79-1bb7c488974d>
2013-04-26 07:58:12 +0000

    Added XML initialization

--- trunk/controller/src/com/jhu/ads/controller/Constants.java
+++ trunk/controller/src/com/jhu/ads/controller/Constants.java
@@ -2,12 +2,10 @@ package com.jhu.ads.controller;
 
 public interface Constants {
 
-	public static String MAIN_TAG = "WowzaServerConfig";
-
-	public static String WOWZA_SERVER_LIST_TAG = "WowzaServerList";
+	public static String WOWZA_SERVER_LIST_TAG = "WowzaServersList";
 	public static String WOWZA_SERVER_TAG = "WowzaServer";
 	public static String WOWZA_SERVER_ID_TAG = "Id";
 	public static String WOWZA_SERVER_IP_TAG = "Ip";
-	public static String WOWZA_SERVER_CAPACITY_TAG = "Capacity";
+	public static String WOWZA_SERVER_INITIAL_CAPACITY_TAG = "InitialCapacity";
 
 }
