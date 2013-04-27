d665554cbd08fe565586b60078be64b35cf4cc71
p.adityarao@gmail.com <p.adityarao@gmail.com@817decba-a3b9-ad42-0e79-1bb7c488974d>
2013-04-27 09:36:20 +0000

    Implemented TokenMgr + Changes in TokenInfo.

--- trunk/controller/src/com/jhu/ads/controller/TokenInfo.java
+++ trunk/controller/src/com/jhu/ads/controller/TokenInfo.java
@@ -1,9 +1,28 @@
 package com.jhu.ads.controller;
 
-import java.util.LinkedList;
+import java.util.TreeMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class TokenInfo {
 
-	int lastExpiredToken;
-	LinkedList<Token> tokenList = new LinkedList<Token>();
+	private int lastExpiredToken;
+	private Lock lock;
+	public Lock getLock() {
+		return lock;
+	}
+	TreeMap<Integer , Token> tokenList;
+	
+	public TokenInfo() {
+		tokenList = new TreeMap<Integer , Token>();
+		lastExpiredToken = 0;
+		lock = new ReentrantLock();
+	}
+	
+	public int getLastExpiredToken() {
+		return lastExpiredToken;
+	}
+	public void setLastExpiredToken(int lastExpiredToken) {
+		this.lastExpiredToken = lastExpiredToken;
+	}
 }
--- trunk/controller/src/com/jhu/ads/controller/TokenMgr.java
+++ trunk/controller/src/com/jhu/ads/controller/TokenMgr.java
@@ -2,8 +2,11 @@ package com.jhu.ads.controller;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.NavigableSet;
+import java.util.TreeMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.commons.lang.SerializationUtils;
@@ -14,6 +17,7 @@ import spread.SpreadException;
 import spread.SpreadGroup;
 import spread.SpreadMessage;
 
+import com.jhu.ads.common.TokenRequestMsg;
 import com.jhu.ads.common.TokenResponseMsg;
 import com.jhu.ads.controller.common.ConfigMgr;
 
@@ -21,12 +25,11 @@ public class TokenMgr implements AdvancedMessageListener, Runnable {
 	// Add the new token to list of corresponding Web server
 	// A thread to clean up expired tokens
 	private SpreadConnection connection;
-    private AtomicInteger tokenCount;  // modify this inside sync
-	
+	private AtomicInteger remainingTokenCount; // modify this inside sync
 	String GLOBAL_SPREAD_GROUP_NAME = "GLOBAL_GROUP";
-
+	Thread recoverUnusedTokens = null;
 	HashMap<String, TokenInfo> tokenHolder = new HashMap<String, TokenInfo>();
-	HashMap<SpreadGroup, Integer> webserveTokenCount = new HashMap<SpreadGroup, Integer>();
+	HashMap<String, Integer> webserveTokenCount = new HashMap<String, Integer>();
 
 	private static TokenMgr _instance = null;
 
@@ -61,91 +64,169 @@ public class TokenMgr implements AdvancedMessageListener, Runnable {
 			e.printStackTrace();
 			System.exit(1);
 		}
-
 		// Join the global group
 		SpreadGroup globalGroup = new SpreadGroup();
 		try {
 			globalGroup.join(connection, GLOBAL_SPREAD_GROUP_NAME);
+			// TODO: Do the local datacenter spread group
 		} catch (SpreadException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		System.out.println("Joined " + globalGroup + ".");
 
 		// Set the total token count to the total wowza server capacity
-		tokenCount.set(DataCenterMgr.getInstance().determineCurrentCapacity());
+		remainingTokenCount.set(DataCenterMgr.getInstance()
+				.determineCurrentCapacity());
+		recoverUnusedTokens = new Thread(TokenMgr.getInstance());
+		recoverUnusedTokens.start();
 	}
 
 	/*
-	 *  Identify the wowza media server and webserver membership change and set the flag accordingly
+	 * Identify the wowza media server and webserver membership change and set
+	 * the flag accordingly
 	 */
 	@Override
-	public void membershipMessageReceived(SpreadMessage membershipMsg) {  
-		//Currently ignoring all membership messages
+	public void membershipMessageReceived(SpreadMessage membershipMsg) {
+		// Currently ignoring all membership messages
 	}
 
 	@Override
-	public void regularMessageReceived(SpreadMessage regularMsg) { // handle token requests
-		sendTokens(regularMsg.getSender());
+	public void regularMessageReceived(SpreadMessage regularMsg) {
+		// handle token requests
+		TokenRequestMsg tokenRequestMsg = (TokenRequestMsg) SerializationUtils
+				.deserialize(regularMsg.getData());
+		sendTokens(tokenRequestMsg.getWebserverName()); // send the tokens
 	}
-	
-	public void sendTokens(SpreadGroup sender) {
-    	TokenResponseMsg tokenResponseMsg = new TokenResponseMsg();
-    	int currentBatchCount, newTokenCount;
-		if(tokenCount.get() < Integer.parseInt(ConfigMgr.getInstance().getTokenBatchCount())){
-			currentBatchCount = tokenCount.get();
-		}else{
-			currentBatchCount = Integer.parseInt(ConfigMgr.getInstance().getTokenBatchCount());
+
+	public void sendTokens(String webserverName) {
+		TokenResponseMsg tokenResponseMsg = new TokenResponseMsg();
+		int currentBatchCount, newTokenCount;
+		if (remainingTokenCount.get() < ConfigMgr.getInstance()
+				.getTokenBatchCount()) {
+			currentBatchCount = remainingTokenCount.get(); // Give away
+															// everything
+		} else {
+			currentBatchCount = ConfigMgr.getInstance().getTokenBatchCount();
 		}
-		if(!webserveTokenCount.containsKey(sender)){
-			webserveTokenCount.put(sender, currentBatchCount);
-		}else{
-			newTokenCount = webserveTokenCount.get(sender) + currentBatchCount;
-			webserveTokenCount.put(sender, newTokenCount);
+		remainingTokenCount.addAndGet(-currentBatchCount);
+
+		Integer currentCount = webserveTokenCount.get(webserverName);
+		newTokenCount = (currentCount == null ? 0 : currentCount)
+				+ currentBatchCount;
+		webserveTokenCount.put(webserverName, newTokenCount);
+
+		// Add the tokens to the list
+		int i = 0;
+		TokenInfo webserverTokenInfo = tokenHolder.get(webserverName);
+		if (webserverTokenInfo == null) {
+			webserverTokenInfo = new TokenInfo();
+			tokenHolder.put(webserverName, webserverTokenInfo);
 		}
-		tokenCount.addAndGet(-currentBatchCount);
-    	tokenResponseMsg.setMaxCount(webserveTokenCount.get(sender));  
-    	byte[] data = SerializationUtils.serialize(tokenResponseMsg);
-    	SpreadMessage message = new SpreadMessage();
-    	message.setData(data);
-    	message.addGroup(sender);
-    	message.setReliable();
-    	// TODO: Add Type of message
-    	try {
+
+		webserverTokenInfo.getLock().lock();
+		try {
+			while (i < currentBatchCount) {
+				webserverTokenInfo.tokenList.put(currentCount + i, new Token());
+				i++;
+			}
+		} finally {
+			webserverTokenInfo.getLock().unlock();
+		}
+
+		tokenResponseMsg.setMaxCount(webserveTokenCount.get(webserverName));
+		byte[] data = SerializationUtils.serialize(tokenResponseMsg);
+		SpreadMessage message = new SpreadMessage();
+		message.setData(data);
+		message.addGroup(webserverName);
+		message.setReliable();
+		// TODO: Add Type of message
+		try {
 			connection.multicast(message);
 		} catch (SpreadException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    }
-	
+	}
+
 	public void handleToken(String webserver_tokenID) {
-		//Check if the token is valid
-		String sender = webserver_tokenID.substring(0,webserver_tokenID.lastIndexOf("_"));  
-		String tokenIDStr = webserver_tokenID.substring(sender.indexOf("_")+1);
-		Token token = new Token();
+		// Check if the token is valid
+		String sender = webserver_tokenID.substring(0,
+				webserver_tokenID.lastIndexOf("_"));
+		String tokenIDStr = webserver_tokenID
+				.substring(sender.indexOf("_") + 1);
 		int tokenID = Integer.parseInt(tokenIDStr);
-		if( tokenID > tokenHolder.get(sender).lastExpiredToken){
-			token.setRecvdTime(System.currentTimeMillis());
-			token.setTokenId(tokenID);
-			//get current index
-				
-//			tokenHolder.get(sender[0].toString()).tokenList.add(index, token);	
+		TokenInfo tokenInfo1 = tokenHolder.get(sender);
+		if (tokenInfo1 == null || tokenID < tokenInfo1.getLastExpiredToken()) {
+			try {
+				throw new Exception("Token " + tokenID + " Expired");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
+
+		Token token = tokenInfo1.tokenList.get(tokenID);
+		token.setTokenId(tokenID);
+		token.setRecvdTime(System.currentTimeMillis());
+		// exists in the list
+		addToList(token, tokenInfo1.tokenList);
+	}
+
+	private void addToList(Token token, TreeMap<Integer, Token> tokenList) {
+		tokenList.get(token.getTokenId()).setRecvdTime(
+				System.currentTimeMillis());
 	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		for (;;) {
+			int unusedTokenCount = 0;
+			Collection<TokenInfo> values = tokenHolder.values();
+			for (Iterator iterator = values.iterator(); iterator.hasNext();) {
+				TokenInfo tokenInfo = (TokenInfo) iterator.next();
+				NavigableSet<Integer> tokenKeys = tokenInfo.tokenList
+						.navigableKeySet();
 
+				Iterator iterator2 = tokenKeys.descendingIterator();
+				Token currentToken = null;
+				while (iterator2.hasNext()) {
+					currentToken = (Token) iterator2.next();
+					if ((System.currentTimeMillis() - tokenInfo.tokenList.get(
+							currentToken).getRecvdTime()) > ConfigMgr.getInstance().getTokenExpiryTime()) {
+						break;
+					}
+				}
+				boolean isFirst = true;
+				tokenInfo.getLock().lock();
+				try {
+					while (iterator2.hasNext()) {
+						currentToken = (Token) iterator2.next();
+						if (isFirst) {
+							tokenInfo.setLastExpiredToken(currentToken
+									.getTokenId());
+							isFirst = false;
+						}
+						if (currentToken.getRecvdTime() == 0) {
+							unusedTokenCount++;
+						}
+						tokenInfo.tokenList.remove(currentToken.getTokenId());
+					}
+				} finally {
+					tokenInfo.getLock().unlock();
+				}
+				remainingTokenCount.getAndAdd(unusedTokenCount);
+			}
+			try {
+				Thread.sleep(ConfigMgr.getInstance().getTokenExpiryPollingInterval());
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 	public AtomicInteger getTokenCount() {
-		return tokenCount;
+		return remainingTokenCount;
 	}
 
 	public void setTokenCount(AtomicInteger tokenCount) {
-		this.tokenCount = tokenCount;
+		this.remainingTokenCount = tokenCount;
 	}
-
 }
