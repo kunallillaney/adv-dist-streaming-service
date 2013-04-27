c2931e9e5bbd5498cd977eb515ae11be402d713a
raghuram.sv@gmail.com <raghuram.sv@gmail.com@817decba-a3b9-ad42-0e79-1bb7c488974d>
2013-04-27 09:49:30 +0000

    Added Servlets

---
+++ controller/src/com/jhu/ads/controller/servlet/InitServlet.java
@@ -0,0 +1,46 @@
+package com.jhu.ads.controller.servlet;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+import com.jhu.ads.controller.DataCenterMgr;
+import com.jhu.ads.controller.TokenMgr;
+import com.jhu.ads.controller.common.ConfigMgr;
+
+@WebServlet( name="InitServlet", displayName="Initialization Servlet", urlPatterns = {"/init"}, loadOnStartup=0)
+public class InitServlet extends HttpServlet {
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        
+        String dHome = System.getenv("DHOME");
+        if(dHome == null || dHome.equals("")) {
+            throw new ServletException("DHOME environment variable NOT set.");
+        }
+        
+        ConfigMgr.getInstance().init(dHome + "/conf/datacenter-config.properties");
+        
+        InputStream wowzaServerConfiguration;
+        try {
+            wowzaServerConfiguration = new FileInputStream(dHome + "/conf/wowzaservers-config.xml");
+            DataCenterMgr.getInstance().init(wowzaServerConfiguration);
+            wowzaServerConfiguration.close();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+        TokenMgr.getInstance().init();
+    }
+    
+}
--- controller/src/com/jhu/ads/controller/servlet/UserRequestServlet.java
+++ controller/src/com/jhu/ads/controller/servlet/UserRequestServlet.java
@@ -1,11 +1,17 @@
 package com.jhu.ads.controller.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.jhu.ads.controller.DataCenterMgr;
+import com.jhu.ads.controller.TokenMgr;
+import com.jhu.ads.controller.WowzaServer;
 
 /**
  * Servlet implementation class UserRequestServlet
@@ -27,6 +33,20 @@ public class UserRequestServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	    String tokenStr = request.getParameter("Token");
+	    TokenMgr.getInstance().handleToken(tokenStr);
+	    
+	    WowzaServer wowzaServer = DataCenterMgr.getInstance().assignWowzaServer();
+	    String wowzaIp = wowzaServer.getWowzaIp();
+	    
+        response.setContentType("text/html");
+        PrintWriter printWriter  = response.getWriter();
+
+        printWriter.println("<embed width='768' height='456' " +
+                		    "src='http://www.focusonthefamily.com/family/JWPlayer/mediaplayer.swf'        " +
+                		    "flashvars='file=sample.mp4&streamer=rtmp://"+wowzaIp+"/vod/'" +
+                		    "/>");
+        printWriter.println("<h2>Video is being served from "+wowzaIp+"</h2>");
+	    
 	}
 }
