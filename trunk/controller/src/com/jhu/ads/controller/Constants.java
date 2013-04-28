9e60be8b075f5d037eeb7cb61f18288eac085de5
raghuram.sv@gmail.com <raghuram.sv@gmail.com@817decba-a3b9-ad42-0e79-1bb7c488974d>
2013-04-28 02:40:26 +0000

    Added maxCapacity and changed the behavior of getWowzaCount API

--- trunk/controller/src/com/jhu/ads/controller/Constants.java
+++ trunk/controller/src/com/jhu/ads/controller/Constants.java
@@ -6,7 +6,7 @@ public interface Constants {
 	public static String WOWZA_SERVER_TAG = "WowzaServer";
 	public static String WOWZA_SERVER_ID_TAG = "Id";
 	public static String WOWZA_SERVER_IP_TAG = "Ip";
-	public static String WOWZA_SERVER_INITIAL_CAPACITY_TAG = "InitialCapacity";
+	public static String WOWZA_SERVER_MAX_CAPACITY_TAG = "MaxCapacity";
 	
 	public static String WOWZA_MEDIA_SERVER_TAG = "WowzaMediaServer";
 	public static String WOWZA_SERVER_COUNT_TAG = "ConnectionsCurrent";
