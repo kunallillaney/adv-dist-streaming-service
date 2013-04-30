f8d54ad952affa06e73ad24b3bc373e98acecf00
raghuram.sv@gmail.com <raghuram.sv@gmail.com@817decba-a3b9-ad42-0e79-1bb7c488974d>
2013-04-30 01:36:15 +0000

    Added wowza streamingPort

--- controller/src/com/jhu/ads/controller/Constants.java
+++ controller/src/com/jhu/ads/controller/Constants.java
@@ -6,6 +6,7 @@ public interface Constants {
 	public static String WOWZA_SERVER_TAG = "WowzaServer";
 	public static String WOWZA_SERVER_ID_TAG = "Id";
 	public static String WOWZA_SERVER_IP_TAG = "Ip";
+	public static String WOWZA_SERVER_STREAMING_PORT_TAG = "StreamingPort";
 	public static String WOWZA_SERVER_MAX_CAPACITY_TAG = "MaxCapacity";
 	
 	public static String WOWZA_MEDIA_SERVER_TAG = "WowzaMediaServer";
--- controller/src/com/jhu/ads/controller/WowzaServer.java
+++ controller/src/com/jhu/ads/controller/WowzaServer.java
@@ -5,6 +5,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class WowzaServer {
 	private String wowzaId;
 	private String wowzaIp;
+	private int streamingPort;
 	private AtomicInteger currentCapacity = new AtomicInteger();
 	private int maxCapacity;
 
@@ -20,10 +21,18 @@ public class WowzaServer {
 		return wowzaIp;
 	}
 
-	public void setWowzaIp(String wowzaIp) {
+    public void setWowzaIp(String wowzaIp) {
 		this.wowzaIp = wowzaIp;
 	}
+    
+    public int getStreamingPort() {
+        return streamingPort;
+    }
 
+    public void setStreamingPort(int streamingPort) {
+        this.streamingPort = streamingPort;
+    }
+    
 	public AtomicInteger getCurrentCapacity() {
 		return currentCapacity;
 	}
