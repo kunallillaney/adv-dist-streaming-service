5852147c2e0666cd2b2f6a01bd0ca03b08ad1962
raghuram.sv@gmail.com <raghuram.sv@gmail.com@817decba-a3b9-ad42-0e79-1bb7c488974d>
2013-05-02 01:23:04 +0000

    Controller page now displays user info

--- trunk/controller/src/com/jhu/ads/controller/servlet/UserRequestServlet.java
+++ trunk/controller/src/com/jhu/ads/controller/servlet/UserRequestServlet.java
@@ -2,6 +2,7 @@ package com.jhu.ads.controller.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.URLDecoder;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,6 +10,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.SerializationUtils;
+import org.bouncycastle.util.encoders.Base64;
+
+import com.jhu.ads.common.UserInfo;
 import com.jhu.ads.controller.DataCenterMgr;
 import com.jhu.ads.controller.TokenMgr;
 import com.jhu.ads.controller.WowzaServer;
@@ -55,6 +60,22 @@ public class UserRequestServlet extends HttpServlet {
                 		    "flashvars='autostart=true&allowfullscreen=true&file=sample.mp4&streamer=rtmp://"+wowzaIp+":"+wowzaStreamingPort+"/vod/'" +
                 		    "/>");
         printWriter.println("<h2>Video is being served from "+wowzaIp+":"+wowzaStreamingPort+"</h2>");
+        
+        String userInfoStr = request.getParameter("UserInfo");
+        if(userInfoStr!=null && !userInfoStr.equals("") ) {
+            UserInfo userInfo = (UserInfo) SerializationUtils.deserialize(Base64.decode(userInfoStr.getBytes()));
+            printWriter.println("<h3>More details about Client</h3>");
+            printWriter.println("<table>");
+            printWriter.println("<tr> <td> Client Addr: </td> <td>" + request.getRemoteAddr() + "</td> </tr>");
+            printWriter.println("<tr> <td> Latitude: </td> <td>" + userInfo.getLatitude() + "</td> </tr>");
+            printWriter.println("<tr> <td> Longitude: </td> <td>" + userInfo.getLongitude() + "</td> </tr>");
+            printWriter.println("<tr> <td> Zipcode: </td> <td>" + userInfo.getPostalCode() + "</td> </tr>");
+            printWriter.println("<tr> <td> City: </td> <td>" + userInfo.getCityName() + "</td> </tr>");
+            printWriter.println("<tr> <td> Country: </td> <td>" + userInfo.getCountryName() + "</td> </tr>");
+            printWriter.println("<tr> <td> Token Being Used: </td> <td>" + tokenStr + "</td> </tr>");
+            printWriter.println("</table>");
+        }
+        
 	    
 	}
 }
